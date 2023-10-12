be23330e23f39ee6f7fad53b31e793b34273e710
34611 <34611@AJARE>
2023-10-12 18:52:36 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio3Inicial/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio3Inicial
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Ejercicio4Inicial/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Ejercicio4Inicial
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+   
+        private void DatePickerEntrada_OnDateSelected(object sender, DateChangedEventArgs e)
+        {
+            // Cuando se selecciona una fecha de entrada, habilitar el DatePicker de salida
+            datePickerSalida.IsEnabled = true;
+        }
+    }
+}
